dacc4d817ed1cb1f1e5369efd8777880d3394ee7
Luis Diaz <luisvmt@hotmail.com>
2026-03-24 21:16:40 -0500

    ok

--- suma.py
+++ suma.py
@@ -1,5 +1,11 @@
+
+#esta es una suma de dos numeros
 def suma(a, b):
     return a + b
+
+#esta es una resta de dos numeros
+def resta(a, b):
+    return a - b
 
 resultado = suma(5, 3)
 print(f"El resultado de la suma es: {resultado}")
